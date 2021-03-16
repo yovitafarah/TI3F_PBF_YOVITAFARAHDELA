93766c65bee1d3c4f0a2e92ba4c3b92da4728bdd
yovitafarah <70680971+yovitafarah@users.noreply.github.com>
2021-03-16 13:01:45 +0700

    Add files via upload
    js6

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => 
+f)(createStore)(MainReducer)
+
+ReactDOM.render(<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
+
+
+registerServiceWorker();
